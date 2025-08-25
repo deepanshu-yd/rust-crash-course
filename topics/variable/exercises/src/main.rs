a356875fdbbc8f2dcb081e5ed45e4225c4d09db5
deepanshu <yadavdeepanshu000@gmail.com>
2025-08-25 20:16:14 +0530

    variable exercise solved

--- topics/variable/exercises/src/main.rs
+++ topics/variable/exercises/src/main.rs
@@ -2,8 +2,8 @@
 // Exercise: Fix the code to make it compile and pass the assertions
 
 fn main() {
-    // Exercise 1: Make this variable mutable
-    let count = 1;
+    // Exercise 1: Make this variable mutable - Solved!
+    let mut count = 1;
     count += 1;
 
     println!("count: {count}");
